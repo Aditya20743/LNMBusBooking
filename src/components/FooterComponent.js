a69fbd501dfe12f6edde42a097b6bb6e6cafde80
Aditya <khadekaraditya20743@gmail.com>
2022-11-06 23:32:28 +0530

    Pending Header and Footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,7 +3,78 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-            <div>Code</div>
+            <footer>
+                <div className='container'>
+                {/* <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top ml-auto"> */}
+                    <div className='row'>
+                    <div className="col-3 mb-3">
+                        <a
+                            className="d-flex align-items-center mb-0 link-dark text-decoration-none"
+                        >
+                           <img src="assests/images/footer-logo.png" alt="LNMBus" width="350" height="135" className="mt-1 py-4 pl-5" />
+
+                        </a>
+                        {/* <p className="text-muted">© 2022</p> */}
+
+                        
+                    </div>
+                    <div className="col mb-3 "></div>
+                    <div className="col mb-3">
+                        <ul className="nav flex-column ">
+                            <li className="nav-item mb-2">
+                                <a href="#" className="nav-link p-0 text-white">
+                                    Refund Rules
+                                </a>
+                            </li>
+                            <li className="nav-item mb-2">
+                                <a href="#" className="nav-link p-0 text-white">
+                                    Terms & Conditions
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div className="col mb-3">
+                        
+                        <ul className="nav flex-column">
+                            
+                            <li className="nav-item mb-2">
+                                <a href="#" className="nav-link p-0 text-white">
+                                    Need Help?
+                                </a>
+                            </li>
+                            <li className="nav-item mb-2">
+                                <a href="#" className="nav-link p-0 text-white">
+                                    FAQs
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col mb-3">
+                        
+                        <ul className="nav flex-column">
+                            
+                            <li className="nav-item mb-2">
+                                <a href="#" className="nav-link p-0 text-white">
+                                    About
+                                </a>
+                            </li>
+                            <li className="nav-item mb-2">
+                                <a href="#" className="nav-link p-0 text-white">
+                                    Contact Us
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </footer>
+
+
+
+
+
         )
     }
 }
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,74 +1,107 @@
 import React, { Component } from 'react'
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import WalletOutlinedIcon from '@mui/icons-material/WalletOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 //add bootstrap links
 
 class Header extends Component {
   render() {
     return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                  <a className="navbar-brand" href="/">
-                  Navbar
-                  </a>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
+        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+          <div className='container-fluid  mx-5'> 
+          <a className="navbar-brand" href="/">
+            <img src="assests/images/logo.png" alt="LNMBus" width={150} height="150" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item ">
+                <p className="nav-link fs-5" href="#">
+                  Home <span className="sr-only">(current)</span>
+                </p>
+              </li>
+              <li className="nav-item  "  >
+                <a className="nav-link" href="#">
+                  View Schedule <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Request Bus   <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  data-toggle="dropdown"
                 >
-                
-                  <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                      <a className="nav-link" href="#">
-                        Home <span className="sr-only">(current)</span>
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        Link
-                      </a>
-                    </li>
-                    <li className="nav-item dropdown">
-                      <a
-                        className="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdown"
-                        role="button"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        Dropdown
-                      </a>
-                      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">
-                          Action
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Another action
-                        </a>
-                        <div className="dropdown-divider" />
-                        <a className="dropdown-item" href="#">
-                          Something else here
-                        </a>
-                      </div>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link disabled" href="#">
-                        Disabled
-                      </a>
-                    </li>
-                  </ul>
+                  Manage Booking
                 </div>
-              </nav>     
-            </div>
-        )
-    }
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">
+                    Book Ticket
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Cancel Ticket
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    View Trips
+                  </a>
+
+
+                </div>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link" href="#">
+                  <QrCodeScannerIcon /> <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link" href="#">
+                  <WalletOutlinedIcon /> <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+              <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  data-toggle="dropdown"
+                >
+                  <AccountCircleIcon  fontSize='large'/> <span className="sr-only">(current)</span>
+                </div>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">
+                    Profile
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Log Out
+                  </a>
+                </div>
+              </li>
+
+
+
+            </ul>
+          </div>
+          </div>
+        </nav>
+    )
+  }
 }
 
 export default Header
+
+// add image icon of different users
