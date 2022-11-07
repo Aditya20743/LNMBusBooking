bd0af0fed8d3114cddbdd4d23dc8480488ddaab7
Aditya <khadekaraditya20743@gmail.com>
2022-11-07 19:21:31 +0530

    Finally Completed Header and Footer

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-//import { ConfigureStore } from './redux/configureStore';
+import { ConfigureStore } from './redux/configureStore';
 
 
-//const store = ConfigureStore();
+const store = ConfigureStore();
 
 function App() {
   return (
-    //<Provider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <div>
           <Main/>
         </div>
       </BrowserRouter>
-    //</Provider>
+    </Provider>
   );
 }
 
