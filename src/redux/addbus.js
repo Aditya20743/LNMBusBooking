1f9d9f99f6703aef4c5590e78165b7e9e341225a
Atharva-214 <20ucs041@lnmiit.ac.in>
2022-11-11 11:04:48 +0530

    Modified requested changes in AddBus

--- src/redux/addbus.js
+++ src/redux/addbus.js
@@ -1,27 +1,20 @@
 import * as ActionTypes from './ActionTypes';
  
-// The auth reducer. The starting state sets authentication
-// based on a token being in local storage. In a real app,
-// we would also want a util to check if the token is expired.
 export const AddBus = (state = {
-        isLoading: false,
-        user: null,
+        // user: null,
         errMess: null
     }, action) => {
     switch (action.type) {
         case ActionTypes.ADDBUS_REQUEST:
             return {...state,
-                isLoading: true,
+                // isLoading: true,
             };
-        case ActionTypes.ADDBUS:
+        case ActionTypes.ADDBUS_SUCCESS:
             return {...state,
-                isLoading: false,
                 errMess: '',
-                user: action.user
             };
         case ActionTypes.ADDBUS_FAILURE:
             return {...state,
-                isLoading: false,
                 errMess: action.message
             };
         default:
